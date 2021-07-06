45d207e30ec08dd5ff7f8300700b7934746dd690
asishraz <iamasish3001@gmail.com>
2021-07-06 14:43:27 +0530

    ds_intro section added

---
+++ data_structures_Intro/notes.js
@@ -0,0 +1,54 @@
+{
+  /* 
+
+//? data structure - collection of values and the value can have relationship amongst them
+data structure is like a container or department (e.g a bag, packing box)
+and each container is good for its own thing
+we can not place a liquid food inside a bag and similarly we can't place a shoes inside a fridge
+
+DS is responsible for storing data and retrieving it easily
+
+Block chain, itself is a data structure which holds information
+In daily life, we organize the most of things for easy retrieval 
+
+Data needs to be stored in different format
+
+//* How Computers Store DATA
+
+Variables stored in RAM
+Medial files- VIdeo.music or audio or docs stored in STORAGE
+Data storage is persistent or in simple words, its permanent
+
+
+//*! Then why we don't use STORAGE for storing Variables, as we lose memory in RAM when the computer is switched off?
+//*? CPU accesses the RAM much faster than the STORAGE, therefore if we store the varialbes in STORAGE, it will make the process slow
+//*? We can access any random memory in RAM very fast 
+
+
+//*? 8-bit(11111111) - 255 bits of information;
+//*?  16-bit - 65,536, 32-bit - 2,147,483,674 ; 64-bit - 9,223,372,036,854,775,807
+
+Each data type has a number of bit associated with it and it needs to store in the system
+then system allocate that space and CPU reads that info.
+
+DS- the arrangement of DATA
+we can arrange the data in RAM 
+
+
+//*todo Our goal is to minimize the operation that we need to do for the CPU to get the information.
+//*todo and that is why DS is very powerful
+
+//* THE MAIN DS AND ALGO TO LEARN
+
+//*! Arrays, Trees, Stacks, Tries, Queues, Graphs, LL, Hashtables
+//*! Sorting, DP, BFS+DFS(searching), Recursion
+
+
+
+
+
+
+
+
+ */
+}
